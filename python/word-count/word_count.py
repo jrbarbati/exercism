37fba6f53c938de9e674787a84e6fe30302e8855
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-07-12 11:39:28 -0400

    [#master] completed python/word-count
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

---
+++ python/word-count/word_count.py
@@ -0,0 +1,10 @@
+import re
+from collections import Counter
+
+
+def count_words(sentence: str) -> dict:
+    return Counter(to_word_array(sentence))
+
+
+def to_word_array(sentence: str) -> list:
+	return re.findall('[a-z]+\'[a-z]+|[a-z]+|[1-9]', sentence.lower())
