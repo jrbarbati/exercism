10aaf81c6f927aa90f9d708e29f8148146f18ac9
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-07-20 20:47:17 -0400

    [#master] improving go/darts
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -5,19 +5,16 @@ import "math"
 
 // Score calculates the score of a single throw of a dart
 func Score(x, y float64) int {
-	radius := math.Sqrt(math.Pow(x, 2.0) + math.Pow(y, 2.0))
+	radius := math.Hypot(x, y)
 
-	if radius <= 1 {
+	switch {
+	case radius <= 1:
 		return 10
-	}
-
-	if radius <= 5 {
+	case radius <= 5:
 		return 5
-	}
-
-	if radius <= 10 {
+	case radius <= 10:
 		return 1
+	default:
+		return 0
 	}
-
-	return 0
 }
