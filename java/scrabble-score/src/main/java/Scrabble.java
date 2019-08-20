c77366aa8d171a4695dc29a0f2522d49dce42f2c
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-08-20 10:18:01 -0400

    [#master] refactoring java/scrabble-score for mentors comments, switch-case more efficient than map (in this case).
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,6 +1,3 @@
-import java.util.Map;
-import java.util.HashMap;
-
 class Scrabble
 {
     private Integer score;
@@ -12,40 +9,53 @@ class Scrabble
 
     private Integer calculateScore(String word)
     {
-        return word.chars().map(scores::get).sum();
+        return word.chars().map(this::fetchScoreFor).sum();
+    }
+
+    private int fetchScoreFor(int letter)
+    {
+        switch ((char) letter)
+        {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+        }
     }
 
     int getScore()
     {
         return score;
     }
-
-    private static final Map<Integer, Integer> scores = new HashMap<Integer, Integer>() {{
-        put((int) 'A', 1);
-        put((int) 'B', 3);
-        put((int) 'C', 3);
-        put((int) 'D', 2);
-        put((int) 'E', 1);
-        put((int) 'F', 4);
-        put((int) 'G', 2);
-        put((int) 'H', 4);
-        put((int) 'I', 1);
-        put((int) 'J', 8);
-        put((int) 'K', 5);
-        put((int) 'L', 1);
-        put((int) 'M', 3);
-        put((int) 'N', 1);
-        put((int) 'O', 1);
-        put((int) 'P', 3);
-        put((int) 'Q', 10);
-        put((int) 'R', 1);
-        put((int) 'S', 1);
-        put((int) 'T', 1);
-        put((int) 'U', 1);
-        put((int) 'V', 4);
-        put((int) 'W', 4);
-        put((int) 'X', 8);
-        put((int) 'Y', 4);
-        put((int) 'Z', 10);
-    }};
 }
