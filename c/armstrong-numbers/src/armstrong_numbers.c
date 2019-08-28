6841dfbf18ab64c2e53c8b50c6347c2db210734e
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-08-28 19:10:55 -0400

    [#master] completed c/armstrong-numbers c/leap
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

---
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -0,0 +1,19 @@
+#include "armstrong_numbers.h"
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int isArmstrongNumber(int number)
+{
+	int sum = 0, armstrong_value = number;
+	char digits[100];
+	sprintf(digits, "%d", number);
+
+	while (number != 0)
+	{
+		sum += pow(number%10, strlen(digits));
+		number /= 10;
+	}
+
+	return sum == armstrong_value;
+}
