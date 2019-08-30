12f80ae8362b4a0cbadc24ee801edaa81647ac66
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-08-30 01:11:55 -0400

    [#master] refactoring to not call strlen c/armstrong-numbers
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,14 +1,12 @@
 #include "armstrong_numbers.h"
 #include <math.h>
 #include <stdio.h>
-#include <string.h>
 
 int isArmstrongNumber(int number)
 {
 	int sum = 0, armstrong_value = number, exp;
 	char digits[100];
-	sprintf(digits, "%d", number);
-	exp = strlen(digits);
+	exp = sprintf(digits, "%d", number);
 
 	while (number != 0)
 	{
