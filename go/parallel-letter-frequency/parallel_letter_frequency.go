f2f486049b039139e49b4fae2dce57212f6d7b3e
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-12 13:47:21 -0400

    [#master] completed go/parallel-letter-frequency
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -3,5 +3,28 @@ package letter
 
 // ConcurrentFrequency finds the frequency of runes in parallel for given strings.
 func ConcurrentFrequency(strings []string) FreqMap {
-	return FreqMap{}
+	channel := make(chan FreqMap)
+	frequencies := FreqMap{}
+
+	for _, text := range strings {
+		go countFrequency(text, channel)
+	}
+
+	for i := 0; i < len(strings); i++ {
+		frequencies = combine(frequencies, <-channel)
+	}
+
+	return frequencies
+}
+
+func countFrequency(text string, channel chan FreqMap) {
+	channel <- Frequency(text)
+}
+
+func combine(mainFreqMap, otherFreqMap FreqMap) FreqMap {
+	for key, value := range otherFreqMap {
+		mainFreqMap[key] += value
+	}
+
+	return mainFreqMap
 }
