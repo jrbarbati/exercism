cf9f6eeda6ed69980d532379be7507e003c3fbb9
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-13 10:51:05 -0400

    [#master] completed swift/linked-list
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- swift/linked-list/Sources/LinkedList/LinkedList.swift
+++ swift/linked-list/Sources/LinkedList/LinkedList.swift
@@ -14,18 +14,52 @@ class Deque<T> {
     var tail: Node<T>?
     
     func push(_ data: T) {
+        let newNode = Node(data)
         
+        if self.head == nil && self.tail == nil {
+            self.head = newNode
+            self.tail = newNode
+        } else {
+            self.tail!.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        }
     }
     
     func pop() -> T? {
-        return nil
+        guard let node = self.tail else {
+            return nil
+        }
+        
+        self.tail = node.prev
+        self.tail?.next = nil
+        node.prev = nil
+        
+        return node.data
     }
     
     func unshift(_ data: T) {
+        let newNode = Node(data)
         
+        if self.head == nil && self.tail == nil {
+            self.head = newNode
+            self.tail = newNode
+        } else {
+            self.head!.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+        }
     }
     
     func shift() -> T? {
-        return nil
+        guard let node = self.head else {
+            return nil
+        }
+        
+        self.head = node.next
+        self.head?.prev = nil
+        node.next = nil
+        
+        return node.data
     }
 }
