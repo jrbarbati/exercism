dc1f8ff615159fd6f36fb09e987c6220384f259b
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-13 15:55:09 -0400

    [#master] refactoring go/series to add comments to all exported members
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- go/series/series.go
+++ go/series/series.go
@@ -1,5 +1,7 @@
+// Package series contains methods for creating a series of strings given a length and a string
 package series
 
+// All finds all the series that can be made given the desired length and an inital string of numbers.
 func All(length int, numbers string) []string {
 	if length > len(numbers) {
 		return nil
@@ -11,17 +13,19 @@ func All(length int, numbers string) []string {
 
 	series := make([]string, 0)
 
-	for i := 0; i < len(numbers) - length + 1; i++ {
+	for i := 0; i < len(numbers)-length+1; i++ {
 		series = append(series, numbers[i:length+i])
 	}
 
 	return series
 }
 
+// UnsafeFirst unsafely finds the first instance of a string with the desired length from the string of numbers.
 func UnsafeFirst(length int, numbers string) string {
 	return numbers[0:length]
 }
 
+// First safely finds the first instance of a string with the desired length from the string of numbers.
 func First(length int, numbers string) (string, bool) {
 	if length > len(numbers) {
 		return "", false
