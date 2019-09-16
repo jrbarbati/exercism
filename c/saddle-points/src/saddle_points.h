e76a32330e710b08c242ce25366b55b79d540b4a
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-16 16:09:32 -0400

    [#master] completed c/saddle-points
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

---
+++ c/saddle-points/src/saddle_points.h
@@ -0,0 +1,21 @@
+#ifndef SADDLE_POINTS_H
+#define SADDLE_POINTS_H
+
+#include <stdlib.h>
+
+typedef struct saddle_point
+{
+	int row;
+	int column;
+} saddle_point_t;
+
+
+typedef struct saddle_points
+{
+	int count;
+	saddle_point_t *points;
+} saddle_points_t;
+
+saddle_points_t *saddlePoints(size_t rows, size_t cols, uint8_t matrix[rows][cols]);
+
+#endif
