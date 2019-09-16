c9eb7beb5d120d43e6e06f865298f02c8a491650
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-16 10:44:10 -0400

    [#master] refactoring c/roman-numerals for mentors comments
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- c/roman-numerals/src/roman_numerals.c
+++ c/roman-numerals/src/roman_numerals.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *conversion_numeral[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-const unsigned int conversion_number[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+#ifndef ROMAN_NUMERAL_BUFFER
+#define ROMAN_NUMERAL_BUFFER 101
+#endif
 
 char *to_roman_numeral(unsigned int number)
 {
-	char *roman_numeral = calloc(100, sizeof(char));
+	const char *conversion_numeral[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	const unsigned int conversion_number[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	char *roman_numeral = calloc(ROMAN_NUMERAL_BUFFER, sizeof(char));
 	int index = 0, buffer_pos = 0;
 
-	while (number != 0) 
+	while (number != 0 && buffer_pos < ROMAN_NUMERAL_BUFFER) 
 		if (conversion_number[index] <= number)
 		{
 			sprintf(roman_numeral + buffer_pos, "%s", conversion_numeral[index]);
