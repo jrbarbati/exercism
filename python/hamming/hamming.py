7e3107d03c9885e4053ebe8ee982058c3ce0732f
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-19 14:41:14 -0400

    [#master] updating all completed python problems to newest version
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -3,10 +3,10 @@ def distance(strand_a: str, strand_b: str) -> int:
 	Calculates number of differences (hamming distance) between two equal
 	length strings. Raises ValueError if the strings are different lengths
 	"""
-    if len(strand_a) != len(strand_b):
-    	raise ValueError("Strands must be same length.")
+	if len(strand_a) != len(strand_b):
+		raise ValueError("Strands must be same length.")
 
-    return calculate_distance(strand_a, strand_b)
+	return calculate_distance(strand_a, strand_b)
 
 
 def calculate_distance(strand_a: str, strand_b: str) -> int:
