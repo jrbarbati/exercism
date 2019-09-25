d9a5a4564ec2b43197ef378a2fa7d793be6aaac7
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-09-25 15:51:46 -0400

    [#master] refactoring c/saddle-points, started c/say added x86-64 assembly track, removed mips assembly track
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- c/saddle-points/src/saddle_points.c
+++ c/saddle-points/src/saddle_points.c
@@ -3,12 +3,10 @@
 
 int is_saddle_point(size_t row, size_t col, size_t rows, size_t cols, uint8_t matrix[rows][cols]);
 
-saddle_point_t saddle_point(size_t row, size_t column);
-
 saddle_points_t *saddlePoints(size_t rows, size_t cols, uint8_t matrix[rows][cols])
 {
 	saddle_points_t *saddle_points = calloc(1, sizeof(saddle_points_t));
-	saddle_points->points = calloc(rows * cols, sizeof(saddle_point_t));
+	saddle_points->points = calloc(rows * cols, sizeof(saddle_point_t *));
 	size_t row, col;
 
 	if (matrix == NULL)
@@ -16,29 +14,26 @@ saddle_points_t *saddlePoints(size_t rows, size_t cols, uint8_t matrix[rows][col
 
 	for (row = 0; row < rows; row++)
 		for (col = 0; col < cols; col++)
-            if (is_saddle_point(row, col, rows, cols, matrix))
-                saddle_points->points[saddle_points->count++] = saddle_point(row, col);
+			if (is_saddle_point(row, col, rows, cols, matrix))
+			{
+				saddle_points->points[saddle_points->count].row = row + 1;
+				saddle_points->points[saddle_points->count++].column = col + 1;
+			}
 
 	return saddle_points;
 }
 
 int is_saddle_point(size_t row, size_t col, size_t rows, size_t cols, uint8_t matrix[rows][cols])
 {
-    size_t i;
-    
-    for (i = 0; i < cols; i++)
-        if (matrix[row][col] < matrix[row][i])
-            return 0;
-    
-    for (i = 0; i < rows; i++)
-        if (matrix[row][col] > matrix[i][col])
-            return 0;
-    
-    return 1;
-}
-
-saddle_point_t saddle_point(size_t row, size_t column)
-{
-    saddle_point_t sp = {row + 1, column + 1};
-    return sp;
+	size_t i;
+	
+	for (i = 0; i < cols; i++)
+		if (matrix[row][col] < matrix[row][i])
+			return 0;
+	
+	for (i = 0; i < rows; i++)
+		if (matrix[row][col] > matrix[i][col])
+			return 0;
+	
+	return 1;
 }
---
+++ c/say/src/say.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "say.h"
+
+int say(int64_t input, char **ans)
+{
+	sprintf(*ans, "%s", "one");
+}
