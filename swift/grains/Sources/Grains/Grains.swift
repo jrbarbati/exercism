8e1954d73f97f56fd8d6a4d1a093a584591446dc
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-10-15 15:29:54 -0400

    [#master] refactored swift/grains for mentor comments
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- swift/grains/Sources/Grains/Grains.swift
+++ swift/grains/Sources/Grains/Grains.swift
@@ -1,20 +1,16 @@
-struct Grains {
-    
-    static func square(_ input: Int) throws -> UInt {
-        guard 1 <= input && input <= 64 else {
-            let message = "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
-            throw input > 0 ? GrainsError.inputTooHigh(message) : GrainsError.inputTooLow(message)
-        }
-        
-        return UInt(1) << (input - 1)
+func square(_ input: Int) throws -> UInt {
+    guard 1 <= input && input <= 64 else {
+        let message = "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
+        throw input > 0 ? GrainsError.inputTooHigh(message) : GrainsError.inputTooLow(message)
     }
-    
-    static var total: UInt {
-        return UInt.max
-    }
-    
-    enum GrainsError: Error {
-        case inputTooHigh(String)
-        case inputTooLow(String)
-    }
+    return UInt(1) << (input - 1)
+}
+
+var total: UInt {
+    return UInt.max
+}
+
+enum GrainsError: Error {
+    case inputTooHigh(String)
+    case inputTooLow(String)
 }
