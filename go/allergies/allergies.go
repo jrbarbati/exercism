d5c7892dc3356cccad2bef0bfe69341e3dcb5456
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2019-10-24 15:50:27 -0400

    [#master] moving allergies list/map outside of methods in go/allergies
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,13 +1,25 @@
 //Package allergies contains methods related to finding out to what a patient is allergic.
 package allergies
 
+var allergies = map[string]uint{
+		"eggs":         1,
+		"peanuts":      2,
+		"shellfish":    4,
+		"strawberries": 8,
+		"tomatoes":     16,
+		"chocolate":    32,
+		"pollen":       64,
+		"cats":         128,
+}
+
+var allergiesList = []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
+
 // Allergies takes a score and calculates a list of allergies.
 func Allergies(score uint) []string {
 	allergicTo := make([]string, 0)
-	allergies := []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
 
-	for index, allergy := range allergies {
-		if (1 << index & score) > 0 {
+	for _, allergy := range allergiesList {
+		if AllergicTo(score, allergy) {
 			allergicTo = append(allergicTo, allergy)
 		}
 	}
@@ -17,16 +29,5 @@ func Allergies(score uint) []string {
 
 // AllergicTo uses the score to decide if the patient is allergic to the substance.
 func AllergicTo(score uint, substance string) bool {
-	allergies := map[string]uint{
-		"eggs":         1,
-		"peanuts":      2,
-		"shellfish":    4,
-		"strawberries": 8,
-		"tomatoes":     16,
-		"chocolate":    32,
-		"pollen":       64,
-		"cats":         128,
-	}
-
 	return allergies[substance]&score > 0
 }
