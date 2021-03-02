19d5d35c5ba4bf7aa8c60dea7d9dead26423d9b5
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2021-03-02 18:24:01 -0500

    [#master] updating go/tree-bulding and starting cpp/two-fer
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

---
+++ cpp/two-fer/build/CMakeFiles/two-fer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jxb4933/personal/exercism/cpp/two-fer/test/tests-main.cpp" "/Users/jxb4933/personal/exercism/cpp/two-fer/build/CMakeFiles/two-fer.dir/test/tests-main.cpp.o"
+  "/Users/jxb4933/personal/exercism/cpp/two-fer/two_fer.cpp" "/Users/jxb4933/personal/exercism/cpp/two-fer/build/CMakeFiles/two-fer.dir/two_fer.cpp.o"
+  "/Users/jxb4933/personal/exercism/cpp/two-fer/two_fer_test.cpp" "/Users/jxb4933/personal/exercism/cpp/two-fer/build/CMakeFiles/two-fer.dir/two_fer_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
