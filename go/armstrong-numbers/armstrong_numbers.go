2f9ee592501c048fd04724b87e1361a9bb1e7e0e
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2021-03-01 21:51:30 -0500

    [#master] finishing go/armstrong-numbers
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,3 +1,34 @@
+// Package armstrong contains methods regarding armstrong numbers
 package armstrong
 
+import (
+	"math"
+)
 
+// IsNumber takes a number and returns True if that number is an armstrong number
+// otherwise, False.
+func IsNumber(number int) bool {
+	num := number
+	digits := countNumberOfDigits(number)
+	sum :=  0
+
+	for num != 0 {
+		digit := num % 10
+
+		sum += int(math.Pow(float64(digit), float64(digits)))
+
+		num /= 10
+	}
+
+	return sum == number
+}
+
+
+// countNumberOfDigits finds the number of digits in an integer
+func countNumberOfDigits(number int) int {
+	if number < 10 {
+		return 1
+	}
+
+	return 1 + countNumberOfDigits(number / 10)
+}
