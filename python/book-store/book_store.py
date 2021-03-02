41c00b0b3d1579536571d685c0fdafd46254d994
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2021-03-02 18:55:30 -0500

    [#master] starting python/book-store
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,2 +1,14 @@
-def total(basket: list) -> int:
-    pass
+# One copy of any of the five books costs $8.
+
+# If, however, you buy two different books, you get a 5% discount on those two books.
+
+# If you buy 3 different books, you get a 10% discount.
+
+# If you buy 4 different books, you get a 20% discount.
+
+# If you buy all 5, you get a 25% discount.
+
+BOOK_PRICE = 800
+
+def total(basket):
+	pass
