8b7dd72c608d55c6cc6d7933ffa90a3db96407bd
JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>
2021-03-11 16:17:49 -0500

    [#master] updating go/space-age
    Co-authored-by: JOSEPH BARBATI <JOSEPH_BARBATI1@homedepot.com>

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,18 +1,22 @@
+// Package space calculates the age of each planet relative it's own orbital period.
 package space
 
 type Planet string
 
+const earthOrbialPeriod = 31557600
+
 var ORBITAL_PERIODS = map[Planet]float64 	{
-	Planet("Earth"): 31557600,
-	Planet("Mercury"): 31557600 * 0.2408467,
-	Planet("Venus"): 31557600 * 0.61519726,
-	Planet("Mars"): 31557600 * 1.8808158,
-	Planet("Jupiter"): 31557600 * 11.862615,
-	Planet("Saturn"): 31557600 * 29.447498,
-	Planet("Uranus"): 31557600 * 84.016846,
-	Planet("Neptune"): 31557600 * 164.79132,
+	Planet("Earth"): earthOrbialPeriod,
+	Planet("Mercury"): earthOrbialPeriod * 0.2408467,
+	Planet("Venus"): earthOrbialPeriod * 0.61519726,
+	Planet("Mars"): earthOrbialPeriod * 1.8808158,
+	Planet("Jupiter"): earthOrbialPeriod * 11.862615,
+	Planet("Saturn"): earthOrbialPeriod * 29.447498,
+	Planet("Uranus"): earthOrbialPeriod * 84.016846,
+	Planet("Neptune"): earthOrbialPeriod * 164.79132,
 }
 
+// Age calculates the age of a certain amount of seconds for a given planet.
 func Age(seconds float64, planet Planet) float64 {
 	return seconds / ORBITAL_PERIODS[planet]
 }
